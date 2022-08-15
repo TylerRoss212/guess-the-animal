687caba3caeaf5701bb288c0fd47e4ff2d1e0889
Tyler Ross <Tyler_Ross3@Baylor.edu>
2022-08-15 00:40:34 -0500

    Heroku deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/guess-the-animal'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/guess-the-animal/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
