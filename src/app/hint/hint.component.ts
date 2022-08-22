6a48520e3358a6718a5b74112a3757556707f043
Tyler Ross <Tyler_Ross3@Baylor.edu>
2022-08-22 12:37:41 -0500

    Updated buttons

--- src/app/hint/hint.component.ts
+++ src/app/hint/hint.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ANIMAL_NUMBER_KEY, CURRENT_GUESS_NUM_KEY} from "../constants";
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-hint',
@@ -13,7 +12,11 @@ export class HintComponent implements OnInit {
     animalNumber: Number = 0;
     guessNumber: Number = 0;
 
-    constructor() { }
+    isAudio: boolean = false;
+
+    buttonUrl: String = "assets/icons/play.png";
+
+    constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
     ngOnInit(): void {
         this.getPictureNumber();
@@ -21,7 +24,12 @@ export class HintComponent implements OnInit {
     }
 
     getPictureNumber() {
-        this.pictureNumber = Number(localStorage.getItem(CURRENT_GUESS_NUM_KEY));
+        //this.pictureNumber = Number(localStorage.getItem(CURRENT_GUESS_NUM_KEY));
+        this.pictureNumber = 5;
+        this.guessNumber = this.pictureNumber;
+        if (this.pictureNumber == 1) {
+            this.isAudio = true;
+        }
     }
 
     getAnimalNumber() {
@@ -30,7 +38,7 @@ export class HintComponent implements OnInit {
 
     getUrl(): String {
         let extension = ".png"
-        if (this.pictureNumber == 2) {
+        if (this.pictureNumber == 1) {
             extension = ".mp3";
         }
         return "https://storage.googleapis.com/guesstheanimal/" + this.animalNumber + "/" + this.pictureNumber + extension;
@@ -42,6 +50,17 @@ export class HintComponent implements OnInit {
         }
 
         return false;
+    }
+
+    changeHint(hintNumber: Number) {
+        if (hintNumber == 1) {
+            this.isAudio = true;
+        } else {
+            this.isAudio = false;
+        }
+
+        this.pictureNumber = hintNumber;
+        this.changeDetectorRef.detectChanges();
     }
 
 }
